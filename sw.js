3ba8a7033e0921f9b4cd43a97fe2f0fc3dbae62b
thekunwar12 <148792582+thekunwar12@users.noreply.github.com>
2026-03-22 15:36:36 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+var CACHE_NAME='tika-saathi-v1';
+var urlsToCache=['./','./index.html','./manifest.json','./icons/icon-192.png','./icons/icon-512.png'];
+self.addEventListener('install',function(e){e.waitUntil(caches.open(CACHE_NAME).then(function(c){return c.addAll(urlsToCache)}));self.skipWaiting()});
+self.addEventListener('activate',function(e){e.waitUntil(caches.keys().then(function(n){return Promise.all(n.filter(function(k){return k!==CACHE_NAME}).map(function(k){return caches.delete(k)}))}));self.clients.claim()});
+self.addEventListener('fetch',function(e){e.respondWith(caches.match(e.request).then(function(r){if(r)return r;return fetch(e.request).then(function(nr){if(nr&&nr.status===200){var rc=nr.clone();caches.open(CACHE_NAME).then(function(c){c.put(e.request,rc)})}return nr}).catch(function(){return caches.match('./index.html')})}))});
